3743346704ae3d3ed8d659284931e7ffd8180b7a
Mateusz Salach <w64894@student.wsiz.edu.pl>
2023-03-06 14:16:01 +0100

    zajęcia1

---
+++ lab1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public Address Address { get; set; }
+    }
+}
--- lab1/UnitTest1.cs
+++ lab1/UnitTest1.cs
@@ -8,7 +8,9 @@ namespace lab1
         [Fact]
         public void Test1()
         {
-
+            int i = 0;
+            i++;
+                Console.WriteLine();
         }
     }
 }
